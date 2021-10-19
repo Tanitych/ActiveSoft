59ea079c2b28dd0fffa9b5c5997877a856473fd4
Your NameTanitych <Tanitych@gmail.com>
2021-10-19 18:10:37 +0600

    first repos

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $('.header__burger').click(function (event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+});
+
+$(function () {
+    $("#phone").mask("+7(999) 999-9999");
+});
